5261e4f1fa252ad3724d35fbd0d4d0fd1d71e860
jeff <jeffli678@sina.com>
2018-02-01 21:00:33 -0500

    minimal woring

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 #encoding: utf-8
 import eel
 
+global text
+
 web_app_options = {
 	'mode': "None", #or "chrome"
 	'port': 9600,
 }
+
+@eel.expose
+def update(new_text):
+    text = new_text
+    # print(new_text)
+    eel.show_qrcode(text)
 
 eel.init('web')
 eel.start('index.html', options = web_app_options)
---
+++ web/main.js
@@ -0,0 +1,18 @@
+eel.expose(show_qrcode);
+
+window.onload = show_qrcode;
+
+function show_qrcode(text = 'https://www.google.com')
+{
+  document.getElementById("qrcode").innerHTML = '';
+  document.getElementById("text").value = text;
+  var qrcode = new QRCode("qrcode", {width: 512, height: 512});
+  qrcode.clear();
+  qrcode.makeCode(text);
+}
+
+function update()
+{
+  text = document.getElementById('text').value;
+  eel.update(text);
+}
